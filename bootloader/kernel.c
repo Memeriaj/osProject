951376119572724bb6c3832ca5e7d45fa6e88643
Alex Memering <memeriaj@rose-hulman.edu>
2015-04-15 19:31:36 -0400

    Possibly finihsed the rest of the Milestone
    Still having some issues, the messages aren't displaying on the screen.  We've
    also tried just using the example that was given in the instructions to put one
    white 'a' on the third line and even that isn't displaying.

--- bootloader/kernel.c
+++ bootloader/kernel.c
@@ -1,4 +1,4 @@
-int findAddress(int line, int pos){
+/*int findAddress(int line, int pos){
   return (80*(line-1)+pos)*2+0x8000;
 }
 
@@ -12,10 +12,12 @@ void writeStringToScreen(int line, int startPos, char* str){
     pos += 1;
   }
   return;
-}
+}*/
 
-int main(int argc, char* argv[]){
-
+int main(void){
+  /*writeStringToScreen(2, 2, "Hello World");*/
+  putInMemory(0xB000, 0x8140, 'A');
+  putInMemory(0xB000, 0x8141, 0x7);
 
   while(1){}
   return 0;
