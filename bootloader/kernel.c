ab3fea5012848e38e40214ab2c2f7a0404f60a4c
Alex Memering <memeriaj@rose-hulman.edu>
2015-04-20 09:16:08 -0400

    Finished portion 4 of Milestone 2

--- bootloader/kernel.c
+++ bootloader/kernel.c
@@ -1,27 +1,18 @@
 /*Written by: Austin Fahsl, Alex Memering and Joel Shapiro*/
-int findAddress(int line, int pos);
-void writeStringToScreen(int line, int startPos, char* str);
+void printString(char* message);
 
 int main(){
   //writeStringToScreen(3, 2, "Hello World!");
-  interrupt(0x10, 0xe*256+'Q', 0, 0, 0);
+  printString("Hello World");
 
   while(1){}
   return 0;
 }
 
-int findAddress(int line, int pos){
-  return (80*(line-1)+pos)*2+0x8000;
-}
-
-void writeStringToScreen(int line, int startPos, char* str){
-  int pos = startPos;
-  while(*str != '\0'){
-    int loc = findAddress(line, pos);
-    putInMemory(0xB000, loc, *str);
-    putInMemory(0xB000, loc+1, 0x7);
-    str += 1;
-    pos += 1;
+void printString(char* message){
+  char* current = message;
+  while(*current != '\0'){
+    interrupt(0x10, 0xe*256+*current, 0, 0, 0);
+    current++;
   }
-  return;
 }
