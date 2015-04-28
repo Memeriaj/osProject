b11692f599a8a0fe0012496bfc209bad4eea6e37
Alex Memering <memeriaj@rose-hulman.edu>
2015-04-27 22:42:36 -0400

    Finished portion 6 of Milestone 3

--- bootloader/kernel.c
+++ bootloader/kernel.c
@@ -1,30 +1,34 @@
 /*Written by: Austin Fahsl, Alex Memering and Joel Shapiro*/
+#define MAXFILEENTRY 16
+#define FILEENTRYLENGTH 32
+#define NAMELENGTH 6
+#define FILESECTORLENGTH (FILEENTRYLENGTH - NAMELENGTH)
+#define FILLERSECTOR 0x00
+#define SECTORSIZE 512
+
 void printString(char* message);
 void readString(char* store);
 void readSector(char* buffer, int sector);
+void readFile(char* name, char* buffer);
 void handleInterrupt21(int ax, int bx, int cx, int dx);
 
 int mod(int a, int b);
 int div(int a, int b);
+int matchNames(char* first, char* second, int length);
+void loadFileSectors(char* buffer, char* dir);
 
 int main(){
-  char line[80];
   char buffer[512];
-
-  printString("Enter a line: \0");
-  readString(line);
-  printString(line);
-
-  readSector(buffer, 30);
-  printString(buffer);
-
   makeInterrupt21();
-  interrupt(0x21, 1, line, 0, 0);
-  interrupt(0x21, 0, line, 0, 0);
 
-  while(1){}
+  interrupt(0x21, 3, "messag\0", buffer, 0);
+  interrupt(0x21, 0, buffer, 0, 0);
+
+  while(1);
   return 0;
 }
+
+
 
 void printString(char* message){
   char* current = message;
@@ -61,6 +65,8 @@ void readString(char* store){
   return;
 }
 
+
+
 void readSector(char* buffer, int sector){
   int q;
 
@@ -94,6 +100,52 @@ int div(int a, int b){
 }
 
 
+
+void readFile(char* name, char* buffer){
+  char directory[512];
+  int entry, matched;
+
+  readSector(directory, 2);
+  for(entry=0; entry<MAXFILEENTRY; entry++){
+    matched = matchNames(name, directory+entry*FILEENTRYLENGTH, NAMELENGTH);
+    if(matched == 1){
+      break;
+    }
+  }
+  if(matched == 0){
+    return;
+  }
+
+  loadFileSectors(buffer, directory+entry*FILEENTRYLENGTH);
+
+  return;
+}
+
+int matchNames(char* first, char* second, int length){
+  int namePos;
+  for(namePos=0; namePos<length; namePos++){
+    if(first[namePos] != second[namePos]){
+      return 0;
+    }
+  }
+  return 1;
+}
+
+void loadFileSectors(char* buffer, char* dir){
+  int sector, curSector;
+  for(sector=0; sector<FILESECTORLENGTH; sector++){
+    curSector = *(dir+NAMELENGTH+sector);
+    if(curSector != FILLERSECTOR){
+      readSector(buffer+SECTORSIZE*sector, curSector);
+    }else{
+      return;
+    }
+  }
+  return;
+}
+
+
+
 void handleInterrupt21(int ax, int bx, int cx, int dx){
   switch(ax){
     case 0x0: /*Print String*/
@@ -104,6 +156,9 @@ void handleInterrupt21(int ax, int bx, int cx, int dx){
       break;
     case 0x2: /*Read Sector*/
       readSector(bx, cx);
+      break;
+    case 0x3: /*Read File*/
+      readFile(bx, cx);
       break;
     default:
       printString("Interrupt21 got an ax that was undefined.");
